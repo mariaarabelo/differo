5e6be5bb770d70458ab3982e2e2539c28e16e4b3
Vitor Alves Moreira <vitor.alvesmoreira1@gmail.com>
2023-10-13 17:17:04 +0100

    updates

--- test.pl
+++ test.pl
@@ -1,4 +1,7 @@
-gcd(X, 0, X).
-gcd(X, Y, G):- Y > 0,
-    XMY is X mod Y,
-    gcd(Y, XMY, G).
+% gcd(X, 0, X).
+% gcd(X, Y, G):- Y > 0,
+%     XMY is X mod Y,
+%     gcd(Y, XMY, G).
+
+start:-
+    write('Hello World!').
